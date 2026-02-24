9f11f83f0363a3a70934b8f177eea944ca5cdc3b
jmeza <jmeza@tvmarkets.com>
2026-02-23 20:43:19 -0500

    Creacion Capa Dominio

---
+++ TaskManagement.Domain/Entities/Task.cs
@@ -0,0 +1,38 @@
+﻿using TaskManagement.Domain.Enums;
+namespace TaskManagement.Domain.Entities;
+
+public class TaskItem
+{
+    public Guid Id { get; private set; }
+    public string Title { get; private set; } = string.Empty;
+    public string? Description { get; private set; }
+    public ETaskStatus Status { get; private set; }
+    public Guid UserId { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    private TaskItem() { }
+
+    public TaskItem(string title, string description, Guid userId)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title is required");
+
+        if (userId == Guid.Empty)
+            throw new ArgumentException("User is required");
+
+        Id = Guid.NewGuid();
+        Title = title;
+        Description = description;
+        UserId = userId;
+        Status = ETaskStatus.Pending;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void ChangeStatus(ETaskStatus newStatus)
+    {
+        if (Status == ETaskStatus.Pending && newStatus == ETaskStatus.Done)
+            throw new InvalidOperationException("Cannot change directly from Pending to Done");
+
+        Status = newStatus;
+    }
+}
